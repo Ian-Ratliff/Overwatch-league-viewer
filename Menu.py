167f6cd9d3b562e464cbc2331b5403c83d1b67f7
Ian-Ratliff <49388163+Ian-Ratliff@users.noreply.github.com>
2019-07-10 12:19:03 -0700

    adding my own files

---
+++ Menu.py
@@ -0,0 +1,11 @@
+#Creates a prompt for starting the program
+
+userInfo = []
+print("Welcome to the Overwatch League Viewer, this is a auto watcher. ")
+print("Please provide your twitch username")
+username = input()
+userInfo.append(username)
+print("Please provide twitch password")
+password = input()
+userInfo.append(password)
+
---
+++ browser launch.py
@@ -0,0 +1,5 @@
+import webbrowser
+import selenium
+
+webbrowser.open("https://twitch.tv/overwatchleague",new=1, autoraise=False)
+
